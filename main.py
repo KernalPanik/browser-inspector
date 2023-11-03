a29c07f3e2c828a00299068e86cbcc562722a00e
Dominykas <37594823+Paulikas@users.noreply.github.com>
2023-11-03 12:31:27 +0200

    Speling error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config import PATH_TO_BROWSER
-from browse_inspector import get_hisotry_data
+from browser_inspector import get_hisotry_data
 
 if __name__ == "__main__":
     path_to_root = PATH_TO_BROWSER
