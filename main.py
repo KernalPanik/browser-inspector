29ec67bf144e19e14047b819b45739e7f71e4c43
Lukas Michnevic <lukas.michnevic@protonmail.com>
2023-11-15 19:41:19 +0200

    small improvements to docs and renaming of methods for better consitency

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if __name__ == "__main__":
     #analyzer.analyze_history()
 
     # Some testing of PDF engine, delete later
-    md = start_markdown("testmd.md")
-    append_large_header(md, "Hello world")
-    append_paragraph(md, "Kind of Lorem Ipsum")
-    append_paragraph(md, "paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright")
-    append_small_header(md, "A bit of a smaller header")
-    append_image(md, "test image", "cat.png", "test image", 40, 40)
-    close_markdown(md)
+    md = pdf_start_markdown("testmd.md")
+    pdf_append_large_header(md, "Hello world")
+    pdf_append_paragraph(md, "Kind of Lorem Ipsum")
+    pdf_append_paragraph(md, "paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright paragraph which should be quite large, probably loaded from another file, but for testing it's alright")
+    pdf_append_small_header(md, "A bit of a smaller header")
+    pdf_append_image(md, "test image", "cat.png", "test image", 40, 40)
+    pdf_close_markdown(md)
     pandoc_call("testmd.md", "test-report.pdf", True)
--- pdf_engine.py
+++ pdf_engine.py
@@ -1,5 +1,8 @@
 # A small engine to build the exact pdf we might need.
 # Should support adding paragraphs and pictures
+# It is based on building a markdown and converting it into a pdf using pandoc
+# Please install pandoc using tool-installer script (for macOS or for Windows)
+# on linux, please use your own tools to install (apt install pandoc on Debian)
 
 import subprocess
 import io
@@ -12,21 +15,21 @@ def pandoc_call(test_markdown_path: str, output_file: str, delete_markdown: bool
         os.remove(test_markdown_path)
 
 # Creates a file object which allows to build a markdown
-def start_markdown(filename) -> io.TextIOWrapper:
+def pdf_start_markdown(filename) -> io.TextIOWrapper:
     return open(filename, "w+")
 
-def close_markdown(markdown_file: io.TextIOWrapper):
+def pdf_close_markdown(markdown_file: io.TextIOWrapper):
     markdown_file.close()
 
-def append_large_header(markdown_file: io.TextIOWrapper, header_contents: str):
+def pdf_append_large_header(markdown_file: io.TextIOWrapper, header_contents: str):
     markdown_file.write("# {}\n".format(header_contents))
 
-def append_small_header(markdown_file: io.TextIOWrapper, header_contents: str):
+def pdf_append_small_header(markdown_file: io.TextIOWrapper, header_contents: str):
     markdown_file.write("## {}\n".format(header_contents))
 
-def append_paragraph(markdown_file: io.TextIOWrapper, paragraph_contents: str):
+def pdf_append_paragraph(markdown_file: io.TextIOWrapper, paragraph_contents: str):
     markdown_file.write("{}\n\n".format(paragraph_contents))
 
-def append_image(markdown_file: io.TextIOWrapper, alt_text: str, image_path:str, title: str, width_percent, height_percent):
+def pdf_append_image(markdown_file: io.TextIOWrapper, alt_text: str, image_path:str, title: str, width_percent, height_percent):
     markdown_file.write("![{}]({} \"{}\")".format(alt_text, image_path, title, str(width_percent), str(height_percent)) + "{" + "width={}\% height={}%".format(width_percent, height_percent) + "}\n")
 
