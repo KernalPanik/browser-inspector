8c58ea2ed7bca938efcdf1fa99d3cc2b2a0bd150
Lukas Michnevic <lukas.michnevic@protonmail.com>
2023-11-25 13:14:39 +0200

    Add handling for suspicious website titles

--- browser_inspector.py
+++ browser_inspector.py
@@ -40,25 +40,40 @@ class BrowserInspector():
         return self.visit_infos
 
     '''
-    Locates suspicious site visit information based on provided site names.
+    Locates suspicious site visit information based on provided site names, looking at the website url (site)
     input: a list of sites in a form of domain names
     '''
-    def filter_suspicious_sites(self, profile_root: str, start_timestamp: int, end_timestamp: int, susp_sites: []) -> []:
+    def filter_suspicious_sites_by_url(self, profile_root: str, start_timestamp: int, end_timestamp: int, susp_sites: []) -> []:
         visit_infos: VisitInfo = self.get_history_data(profile_root, start_timestamp, end_timestamp)
         suspicous_visits = []
         for visit_info in visit_infos:
             for site in susp_sites:
                 actual_url = visit_info.url.split("/")[2]
                 if site in actual_url:
+                    # print(visit_info.as_dict)
+                    suspicous_visits.append(visit_info)
+
+        return suspicous_visits
+
+    '''
+    Locates suspicious site visit information based on provided site names, looking at the website title
+    input: a list of sites in a form of domain names
+    '''
+    def filter_suspicious_sites_by_title(self, profile_root: str, start_timestamp: int, end_timestamp: int, susp_keywords: []) -> []:
+        visit_infos: VisitInfo = self.get_history_data(profile_root, start_timestamp, end_timestamp)
+        suspicous_visits = []
+        for visit_info in visit_infos:
+            for site in susp_keywords:
+                if site in visit_info.title.lower():
                     print(visit_info.as_dict)
                     suspicous_visits.append(visit_info)
 
         return suspicous_visits
 
-    def _extract_url_from_visit(self, id: int) -> str:
+    def _extract_url_title_from_visit(self, id: int) -> (str, str):
         for v in self.urls:
             if (v.id == id):
-                return v.url
+                return v.url, v.title
 
     # Finds relevant from_visit entry
     # returns next visit ID and URL
@@ -67,21 +82,22 @@ class BrowserInspector():
             if (visit_id == v.from_visit):
                     return -1, "Unknown"
             if v.id == visit_id:
-                return v.from_visit, self._extract_url_from_visit(v.url)
+                return v.from_visit, self._extract_url_title_from_visit(v.url)
 
         return -1, "Unknown"
     
     def build_sus_link_hierarchy(self, sus_visit_info: VisitInfo) -> []:
-        related_visits = []
+        related_visits = [()]
         for visit in sus_visit_info.visits:
             # For each visit in given visit info, iterate over known visits, 
             # and find related visits to form a sequence of redirects
             #print("visit id: {}, from visit {}".format(visit.id, visit.from_visit))
             vip = visit.from_visit
-            related_visits.append(self._extract_url_from_visit(visit.url))
+            start_url, start_title = self._extract_url_title_from_visit(visit.url)
+            related_visits.append((start_url, start_title))
             while vip != -1:
-                vip, url = self._find_next_visit(vip)
-                related_visits.append(url)
+                vip, visit_data = self._find_next_visit(vip)
+                related_visits.append((visit_data[0], visit_data[1]))
                 #print("linked from {}".format(vip))
 
         return related_visits
